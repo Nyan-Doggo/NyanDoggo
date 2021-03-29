5d79c497be20f21e4f59f7a4bd5776502e3a87b2
GUA1-app <djgrilbu@gmail.com>
2021-03-29 18:52:36 +0200

    blogpost about resuing code

--- utilities/htmlConverter.py
+++ utilities/htmlConverter.py
@@ -2,9 +2,11 @@ textToConvert = ""
 with open("input.txt", "r") as file:
     textToConvert = file.read()
 
+
+textToConvert = textToConvert.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;")
+textToConvert = textToConvert.replace("&", "&amp")
 textToConvert = textToConvert.replace("<", "&lt")
 textToConvert = textToConvert.replace(">", "&gt")
-textToConvert = textToConvert.replace("&", "&amp")
 textToConvert = textToConvert.replace("\n", "<br/>")
 
 with open("output.txt", "w") as file:
